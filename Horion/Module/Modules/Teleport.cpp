b3566cc386e51b01d8587b2299cc43ca3848c6e0
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2021-07-11 16:37:13 +0000

    Reverted teleport

--- Horion/Module/Modules/Teleport.cpp
+++ Horion/Module/Modules/Teleport.cpp
@@ -1,10 +1,8 @@
 #include "Teleport.h"
 
-#include "../../DrawUtils.h"
-
-Teleport::Teleport() : IModule(0, Category::MOVEMENT, "Click a block to and sneak to teleport to it. (Bypass dosent work)") {
+Teleport::Teleport() : IModule(0, Category::MISC, "Click a block to teleport to it") {
 	registerBoolSetting("Only Hand", &this->onlyHand, this->onlyHand);
-	registerBoolSetting("Bypass", &this->bypass, this->bypass);
+	registerBoolSetting("Push", &this->bypass, this->bypass);
 }
 
 Teleport::~Teleport() {
@@ -15,17 +13,17 @@ const char* Teleport::getModuleName() {
 }
 
 void Teleport::onTick(C_GameMode* gm) {
-	if (!GameData::canUseMoveKeys())
-		return;
-	if (onlyHand && g_Data.getLocalPlayer()->getSupplies()->inventory->getItemStack(g_Data.getLocalPlayer()->getSupplies()->selectedHotbarSlot)->item != nullptr)
-		return;
 
+	if (!GameData::canUseMoveKeys()) 
+		return;
+	if (onlyHand && g_Data.getLocalPlayer()->getSupplies()->inventory->getItemStack(g_Data.getLocalPlayer()->getSupplies()->selectedHotbarSlot)->item != nullptr) 
+		return;
+	
 	if (GameData::isRightClickDown() && !hasClicked) {
 		hasClicked = true;
 
 		vec3_ti block = g_Data.getClientInstance()->getPointerStruct()->block;
-		int rayType = g_Data.getClientInstance()->getPointerStruct()->rayHitType;
-		if (rayType != 0) return;
+		if (block == vec3_ti(0, 0, 0)) return;
 		vec3_t pos = block.toFloatVector();
 		pos.x += 0.5f;
 		pos.z += 0.5f;
@@ -35,24 +33,24 @@ void Teleport::onTick(C_GameMode* gm) {
 
 		g_Data.getGuiData()->displayClientMessageF("%sTeleport position set to %sX: %.1f Y: %.1f Z: %.1f%s. Sneak to teleport!", GREEN, GRAY, pos.x, pos.y, pos.z, GREEN);
 	}
-	if (!GameData::isRightClickDown())
+	if (!GameData::isRightClickDown()) 
 		hasClicked = false;
 
-	if (shouldTP && GameData::isKeyDown(VK_SHIFT)) {
+	if (shouldTP && gm->player->isSneaking()) {
 		tpPos.y += (gm->player->getPos()->y - gm->player->getAABB()->lower.y) + 1;  // eye height + 1
 		if (bypass) {
-			int dist2 = (int)gm->player->getPos()->dist(tpPos);
-			int i = (int)dist2 / 5;
+			/*int dist = (int)gm->player->getPos()->dist(tpPos);
+			int i = (int)dist / 5;
 			for (int n = 0; n < i; n++) {
 				vec3_t offs = tpPos.sub(*gm->player->getPos()).div(i).mul(n);
 				C_MovePlayerPacket p = C_MovePlayerPacket(g_Data.getLocalPlayer(), gm->player->getPos()->add(offs));
 				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
 			}
-			gm->player->setPos(tpPos);
+			gm->player->setPos(tpPos);*/
 			float dist = gm->player->getPos()->dist(tpPos);
 			g_Data.getLocalPlayer()->lerpTo(tpPos, vec2_t(1, 1), (int)fmax((int)dist * 0.1, 1));
-		} else
-			gm->player->setPos(tpPos);
+		}
+		else gm->player->setPos(tpPos);
 		shouldTP = false;
 	}
 }
