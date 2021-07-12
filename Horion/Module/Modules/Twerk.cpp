759d9aa39fe7b1f5b69348f7ba7e1f6042a3698b
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2021-07-12 20:05:44 +0000

    Added Test Module For Testing Purposes

--- Horion/Module/Modules/Twerk.cpp
+++ Horion/Module/Modules/Twerk.cpp
@@ -1,29 +1,16 @@
 #include "Twerk.h"
 
-Twerk::Twerk() : IModule(0x0, Category::MOVEMENT, "Crouch Spam") {
-	registerFloatSetting("delay", &this->delay, this->delay, 0.05f, 10);
+Twerk::Twerk() : IModule(0x0, Category::MOVEMENT, "For testing") {
 }
 
 Twerk::~Twerk() {
 }
 
 const char* Twerk::getModuleName() {
-	return ("Twerk");
+	return ("Test Mod");
 }
 
 void Twerk::onTick(C_GameMode* gm) {
-	Odelay++;
-	if (Odelay > delay * 20) {
-		C_GameSettingsInput* input = g_Data.getClientInstance()->getGameSettingsInput();
-		if (gm->player->isSneaking()) {
-			isSneaking = false;
-		} 
-		else {
-			if (!gm->player->isSneaking()) {
-				isSneaking = true;
-			}
-		}
-		g_Data.getClientInstance()->getMoveTurnInput()->isSneakDown = isSneaking;
-		Odelay = 0;
-	}
+	auto player = g_Data.getLocalPlayer();
+	player->addExperience(200);
 }
