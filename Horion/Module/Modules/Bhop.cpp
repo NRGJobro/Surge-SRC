e13f7388c3bc1e4a7f95543bce889500f7d232f0
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2021-08-06 00:14:01 -0400

    Lowhop frequency Slider

--- Horion/Module/Modules/Bhop.cpp
+++ Horion/Module/Modules/Bhop.cpp
@@ -4,7 +4,7 @@ Bhop::Bhop() : IModule(0, Category::MOVEMENT, "Hop around like a bunny!") {
 	registerBoolSetting("Timer", &this->timer, this->timer);
 	registerBoolSetting("LowHop", &this->LowHop, this->LowHop);
 	registerBoolSetting("Speed", &this->bhopspeed, this->bhopspeed);
-	//registerFloatSetting("LowHop Height", &this->hight, this->hight, 1.f, 3.f);
+	registerFloatSetting("LowHop Frequency", &this->hight, this->hight, 0.1f, 1.f);
 	registerFloatSetting("Bhop Speed", &this->speed, this->speed, 0.1f, 0.8f);
 	registerFloatSetting("Timer Speed", &this->tspeed, this->tspeed, 0.21f, 2.f);
 }
@@ -34,7 +34,7 @@ void Bhop::onMove(C_MoveInputHandler* input) {
 
 	if (counter == 1) {
 		if (pressed && LowHop) {
-			player->velocity.y += -0.14;
+			player->velocity.y += -hight;
 		}
 	}
 
